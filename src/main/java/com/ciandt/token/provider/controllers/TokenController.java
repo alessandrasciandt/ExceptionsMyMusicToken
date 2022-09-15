f917ec63d58c1998431120b6352227a9e89e5a17
Alessandra De Souza <107224377+alessandrasciandt@users.noreply.github.com>
2022-09-15 10:40:15 -0400

    Update TokenController.java

--- src/main/java/com/ciandt/token/provider/controllers/TokenController.java
+++ src/main/java/com/ciandt/token/provider/controllers/TokenController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("/api/v1/token")
+@RequestMapping("/v1/token")
 public class TokenController {
 
     private static final Logger logger = LoggerFactory.getLogger(TokenAuthorizerController.class);
